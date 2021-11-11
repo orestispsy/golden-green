dd07125f214f569095ecbb294c94d37d6b8673b0
Orestis Psycharis <opsychar@gmail.com>
2021-11-11 13:30:51 +0100

    set-up

---
+++ client/src/app.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Link, HashRouter } from "react-router-dom";
+
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+            
+        };
+    }
+
+    componentDidMount() {}
+
+
+    render() {
+        return (
+           
+                <div className="appContainer">
+        yolo
+                </div>
+  
+        );
+    }
+}
